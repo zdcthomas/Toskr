4f7e66124da25fffe2931a77534f460103e605f3
zdcthomas <zdcthomas@yahoo.com>
2019-03-12 14:57:07 -0600

    fills out test husks

--- test/health_check_test.exs
+++ test/health_check_test.exs
@@ -2,5 +2,12 @@ defmodule HealthCeckTest do
   use ExUnit.Case
 
   @tag :skip
-  test "it "
+  test "it pings nats repeatedly" do
+  end
+
+  @tag :skip
+  # I don't know how to test this
+  test "it crashes if nats goes down" do
+  end
+
 end
