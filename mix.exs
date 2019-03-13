aaaaba5db4d5c2664f27e8ef4e4df0170f4f7159
zdcthomas <zdcthomas@yahoo.com>
2019-03-13 12:16:07 -0600

    adds pubsub mock to parent app dependency tree

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Toskr.MixProject do
       {:gnat, "~>0.6.0"},
       {:jason, "~> 1.1"},
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
-      {:pubsub, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:pubsub, "~> 1.0.0"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
